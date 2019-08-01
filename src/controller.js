3a39b5c92c36cee0fe6afe79958f6cf697e1c956
VitaliiChm <vitalii.chm@gmail.com>
2019-08-01 23:20:43 +0300

    add filter and sort in backlog

--- src/controller.js
+++ src/controller.js
@@ -1,16 +1,20 @@
 require("@babel/polyfill");
 import Model from './model.js';
 import BacklogPage from './pages/backlog.js';
-import boardPage from './pages/board.js';
+import BoardPage from './pages/board.js';
 import AddTaskPage from './pages/addTask';
-import {ViewAddTask, ViewBacklog } from './view';
+import {ViewAddTask, ViewBacklog, ViewBoard } from './view';
 import { save, load} from './helpers';
 
 const boardNavNode = document.querySelector('[data-role=nav-board]');
 const backlogNavNode = document.querySelector('[data-role=nav-backlog]');
 const addTaskNavNode = document.querySelector('[data-role=nav-task]');
 
-
+const arrowUp = ViewBacklog.getArrowUp();
+const arrowDown = ViewBacklog.getArrowDown();
+const completed = ViewBacklog.getFilterCompl();
+const uncompleted = ViewBacklog.getFilterUncompl();
+const all = ViewBacklog.getFilterAll();
 
 let activeNavNode;
 class Controller {
@@ -64,47 +68,203 @@ function setActiveNavNode(node) {
     activeNavNode.classList.add('active');
 }
 
-function compare( a, b ) {
-    if ( a.title < b.title ){
+function compare( obj1, obj2 ) {
+    if ( obj1.title < obj2.title ){
         return -1;
     }
-    if ( a.title > b.title ){
+    if ( obj1.title > obj2.title ){
         return 1;
     }
     return 0;
 }
 
+function filterCompleted(array){
+    console.log(array);
+    let completedTask = array.filter(function (obj) {
+        return obj.completed === true ? obj: '';
+    });
+console.log(completedTask.length);
+    if (completedTask.length === 0)
+        completedTask.push({title:'Unfortunately, you don\'t have completed tasks :('});
+    return completedTask;
+}
+
+function filterUncompleted(array){
+    console.log(array);
+    let completedTask = array.filter(function (obj) {
+        return obj.completed === false ? obj: '';
+    });
+    console.log(completedTask.length);
+    if (completedTask.length === 0)
+        completedTask.push({title:'Fortunately, you don\'t have uncompleted tasks :)'});
+    return completedTask;
+}
+
+function sortAsc() {
+    BacklogPage.render();
+
+    const arrowUp = ViewBacklog.getArrowUp();
+    const arrowDown = ViewBacklog.getArrowDown();
+    const completed = ViewBacklog.getFilterCompl();
+    const uncompleted = ViewBacklog.getFilterUncompl();
+    const all = ViewBacklog.getFilterAll();
+
+    const state = load();
+
+    state.sort(compare);
+    const model = new Model(state || undefined);
+    model.on('change', state => save(state));
+
+    const view = new ViewBacklog();
+    const controller = new Controller(model, view);
+
+    arrowUp.addEventListener('click', sortDesc);
+    arrowDown.addEventListener('click', sortAsc);
+    completed.addEventListener('click', completedTasks);
+    uncompleted.addEventListener('click', uncompletedTasks);
+    all.addEventListener('click', allTasks);
+}
+
+function sortDesc() {
+    BacklogPage.render();
+
+    const arrowUp = ViewBacklog.getArrowUp();
+    const arrowDown = ViewBacklog.getArrowDown();
+    const completed = ViewBacklog.getFilterCompl();
+    const uncompleted = ViewBacklog.getFilterUncompl();
+    const all = ViewBacklog.getFilterAll();
+
+    const state = load();
+    state.sort(compare);
+    state.reverse();
+    const model = new Model(state || undefined);
+    model.on('change', state => save(state));
+
+    const view = new ViewBacklog();
+    const controller = new Controller(model, view);
+
+
+    arrowUp.addEventListener('click', sortDesc);
+    arrowDown.addEventListener('click', sortAsc);
+    completed.addEventListener('click', completedTasks);
+    uncompleted.addEventListener('click', uncompletedTasks);
+    all.addEventListener('click', allTasks);
+}
+
+function completedTasks() {
+    BacklogPage.render();
+
+    const arrowUp = ViewBacklog.getArrowUp();
+    const arrowDown = ViewBacklog.getArrowDown();
+    const completed = ViewBacklog.getFilterCompl();
+    const uncompleted = ViewBacklog.getFilterUncompl();
+    const all = ViewBacklog.getFilterAll();
+
+    completed.classList.add('active');
+
+    const state = load();
+    const filter = filterCompleted(state);
+    const model = new Model(filter || undefined);
+    model.on('change', state => save(state));
+
+    const view = new ViewBacklog();
+    const controller = new Controller(model, view);
+
+    arrowUp.addEventListener('click', sortDesc);
+    arrowDown.addEventListener('click', sortAsc);
+    completed.addEventListener('click', completedTasks);
+    uncompleted.addEventListener('click', uncompletedTasks);
+    all.addEventListener('click', allTasks);
+}
+
+function uncompletedTasks() {
+    BacklogPage.render();
+
+    const arrowUp = ViewBacklog.getArrowUp();
+    const arrowDown = ViewBacklog.getArrowDown();
+    const completed = ViewBacklog.getFilterCompl();
+    const uncompleted = ViewBacklog.getFilterUncompl();
+    const all = ViewBacklog.getFilterAll();
+
+    uncompleted.classList.add('active');
+
+    const state = load();
+    const filter = filterUncompleted(state);
+    const model = new Model(filter || undefined);
+    model.on('change', state => save(state));
+
+    const view = new ViewBacklog();
+    const controller = new Controller(model, view);
+
+    arrowUp.addEventListener('click', sortDesc);
+    arrowDown.addEventListener('click', sortAsc);
+    completed.addEventListener('click', completedTasks);
+    uncompleted.addEventListener('click', uncompletedTasks);
+    all.addEventListener('click', allTasks);
+}
+
+function allTasks() {
+    BacklogPage.render();
+    const arrowUp = ViewBacklog.getArrowUp();
+    const arrowDown = ViewBacklog.getArrowDown();
+    const completed = ViewBacklog.getFilterCompl();
+    const uncompleted = ViewBacklog.getFilterUncompl();
+    const all = ViewBacklog.getFilterAll();
+
+    all.classList.add('active');
+
+    const state = load();
+    const model = new Model(state || undefined);
+    model.on('change', state => save(state));
+
+    const view = new ViewBacklog();
+    const controller = new Controller(model, view);
+
+    arrowUp.addEventListener('click', sortDesc);
+    arrowDown.addEventListener('click', sortAsc);
+    completed.addEventListener('click', completedTasks);
+    uncompleted.addEventListener('click', uncompletedTasks);
+    all.addEventListener('click', allTasks);
+}
+
 export default {
     async backlogRoute() {
-        const sorted = document.querySelector('.sort');
         BacklogPage.render();
         setActiveNavNode(backlogNavNode);
-        const state = load();
 
+        const arrowUp = ViewBacklog.getArrowUp();
+        const arrowDown = ViewBacklog.getArrowDown();
+        const completed = ViewBacklog.getFilterCompl();
+        const uncompleted = ViewBacklog.getFilterUncompl();
+        const all = ViewBacklog.getFilterAll();
+
+        const state = load();
         const model = new Model(state || undefined);
+
         model.on('change', state => save(state));
 
         const view = new ViewBacklog();
         const controller = new Controller(model, view);
 
-        // sorted.addEventListener('click', () => {
-        //     state.sort(compare);
-        //     BacklogPage.render();
-        //     setActiveNavNode(backlogNavNode);
-        //     const state = load();
-        //
-        //     const model = new Model(state || undefined);
-        //     model.on('change', state => save(state));
-        //
-        //     const view = new ViewBacklog();
-        //     const controller = new Controller(model, view);
-        // });
+        arrowUp.addEventListener('click', sortDesc);
+        arrowDown.addEventListener('click', sortAsc);
+        completed.addEventListener('click', completedTasks);
+        uncompleted.addEventListener('click', uncompletedTasks);
+        all.addEventListener('click', allTasks);
 
     },
+
     async boardRoute() {
-        boardPage.render();
-        setActiveNavNode(boardNavNode);
+        BoardPage.render();
+        setActiveNavNode(addTaskNavNode);
+        const state = load();
+        const model = new Model(state || undefined);
+
+        model.on('change', state => save(state));
+        const view = new ViewBoard();
+        const controller = new Controller(model, view);
     },
+
     async addTaskRoute() {
         AddTaskPage.render();
         setActiveNavNode(addTaskNavNode);
