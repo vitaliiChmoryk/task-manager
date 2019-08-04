32fa889f1b24cbea2f8c009fec2f3c7b745d5613
Vitalii <39777786+vitaliiChmoryk@users.noreply.github.com>
2019-08-04 22:43:53 +0300

    Add files via upload

---
+++ public/landing page/js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $('.slider_container').slick({
+        autoplay: true,
+        autoplaySpeed: 5000,
+        dots: false,
+        speed: 1000,
+        arrows: true,
+    });
+});
+
